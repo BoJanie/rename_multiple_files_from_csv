e4c7b85bb59cefc815069e1800b13b9c97ba3963
BoJanie <41342428+BoJanie@users.noreply.github.com>
2018-07-23 17:02:23 +0000

    Cleaned and added new error
    This version has a section to alert the user if a file from the .csv file is not in the directory.

--- rename_multiple_files_from_csv.py
+++ rename_multiple_files_from_csv.py
@@ -3,13 +3,13 @@ import os, csv, sys
 
 #open and store csv file as a 'dictionary'
 IDs = {}
-#   'rb' = r is for 'reading' the file and 'b' is for binary
 with open('rename_key.csv','rb') as csvfile:
     timeReader = csv.reader(csvfile, delimiter = ',')
     #build dictionary with associated IDs
     for row in timeReader:
         IDs[row[0]] = row[1]
 
+#Can change path name to whatever folder your files to be renamed is.
 path = 'rename_these_files/'
 
 #This section makes sure all files in .csv are in folder. If not, it informs the user.
@@ -26,7 +26,7 @@ for oldname in os.listdir(path):
         old = os.path.join(path, oldname)
         new = os.path.join(path, IDs[oldname])
         os.rename(old, new)
-    #this error section will inform the user if a file in the directory was not in csv
+    #this error section below will inform the user if a file in the directory was not in .csv
     #could also be error in other cases? The lack of .csv was the only one I experienced.
     except KeyError, OSError:
         failed.append(oldname)
